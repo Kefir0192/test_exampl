12410477d84bc809a895fff78b35db518a78db86
KoynovStas <skojnov@yandex.ru>
2016-08-08 11:57:57 +0300

    Tests: list: add test_list_criter

--- tests/list_tests.c
+++ tests/list_tests.c
@@ -627,6 +627,36 @@ int test_list_citer(struct test_info_t  *test_info)
 
 
 
+int test_list_criter(struct test_info_t  *test_info)
+{
+
+    TEST_INIT;
+
+    DECLARE_LIST_HEAD(tmp_list);
+
+    const size_t COUNT_NODES = 100;
+    size_t i;
+    struct tmp_data nodes[COUNT_NODES];
+    struct list_head *it;
+
+
+    for(i=0; i < COUNT_NODES; i++)
+        list_push_front(&nodes[i].list, &tmp_list);
+
+
+    i=0;
+    list_criter(it, &tmp_list)
+    {
+        if( it != &nodes[i].list )  //test nodes
+            return TEST_BROKEN;
+        i++;
+    }
+
+
+    return TEST_PASSED;
+}
+
+
 ptest_func tests[] =
 {
 
@@ -649,6 +679,7 @@ ptest_func tests[] =
 
     //Iterator
     test_list_citer,
+    test_list_criter,
 
 };
 
