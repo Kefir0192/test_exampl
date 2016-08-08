ab1c0dc236f79d7fdc15d2405a01a46dca02ec2b
KoynovStas <skojnov@yandex.ru>
2016-08-08 12:08:55 +0300

    Tests: list: add test_list_data_citer

--- tests/list_tests.c
+++ tests/list_tests.c
@@ -754,6 +754,42 @@ int test_list_riter(struct test_info_t  *test_info)
 
 
 
+int test_list_data_citer(struct test_info_t  *test_info)
+{
+
+    TEST_INIT;
+
+    DECLARE_LIST_HEAD(tmp_list);
+
+    const size_t COUNT_NODES = 100;
+    size_t i;
+    struct tmp_data nodes[COUNT_NODES];
+    struct tmp_data *it;
+
+
+
+    for(i=0; i < COUNT_NODES; i++)
+    {
+        nodes[i].data = i;
+        list_push_back(&nodes[i].list, &tmp_list);
+    }
+
+
+
+    i=0;
+    list_data_citer(it, &tmp_list, struct tmp_data, list)
+    {
+        if( it->data != nodes[i].data )     //test nodes
+            return TEST_BROKEN;
+        i++;
+    }
+
+
+    return TEST_PASSED;
+}
+
+
+
 ptest_func tests[] =
 {
 
@@ -779,6 +815,7 @@ ptest_func tests[] =
     test_list_criter,
     test_list_iter,
     test_list_riter,
+    test_list_data_citer,
 };
 
 
