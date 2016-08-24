fe9461490f482d1e514d83651d15ddb82d18e915
mark <mark@mark.com>
2016-08-24 12:30:19 +0400

    test tree

---
+++ src/arch/ARM_CM3/atomic.h
@@ -0,0 +1,12 @@
+#ifndef ATOMIC_HEADER
+#define ATOMIC_HEADER
+
+
+#include <stddef.h>
+
+
+typedef volatile int atomic_t;
+
+
+
+#endif //ATOMIC_HEADER
---
+++ src/arch/ARM_CM3/sys_timer.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void init_timer()
+{
+  printf("init timer\n");
+}
